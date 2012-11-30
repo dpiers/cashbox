3868927ffc80a8fccc20fba1c3e09c9d09ecf07c
Daniel Piers <danielpiers@gmail.com>
2012-11-30 01:47:03 -0800

    change db user

--- settings.py
+++ settings.py
@@ -13,7 +13,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'cashbox',                      # Or path to database file if using sqlite3.
-        'USER': 'cashbox',                      # Not used with sqlite3.
+        'USER': 'postgres',                      # Not used with sqlite3.
         'PASSWORD': 'CashBox1234CASHBOX!@#$',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.

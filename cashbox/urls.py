2619ef47f6f03f731c992c2b51c57ec32360c205
Daniel Piers <danielpiers@gmail.com>
2012-11-30 03:56:51 -0800

    fix urls

--- cashbox/urls.py
+++ cashbox/urls.py
@@ -3,10 +3,9 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^cashbox/', include('cashbox.foo.urls')),
-    url(r'^$', 'cashbox.views.index', name='index'),
+urlpatterns = patterns(
+    'zerocater.core.views',
+	url(r'^$', 'index', name='index'),
 
     url(r'^admin/', include(admin.site.urls)),
 )
